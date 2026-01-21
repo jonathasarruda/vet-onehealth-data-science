3c14169eb6e196c6bb31d181be6cb5d71df01bbe
Jonathas dos Santos Arruda <39897924+jonathasarruda@users.noreply.github.com>
2026-01-21 13:07:56 -0400

    Update app.py

--- deploy_implantacao/zoonotic_risk_api/app.py
+++ deploy_implantacao/zoonotic_risk_api/app.py
@@ -7,3 +7,10 @@ predictions = {"Farm A": 0.85, "Farm B": 0.40, "Farm C": 0.65}
 @app.get("/predict")
 def get_predictions():
     return predictions
+
+@app.get("/")
+def root():
+    return {
+        "message_en": "Zoonotic Risk API is running",
+        "message_pt": "API de Risco Zoonótico está em execução"
+    }
